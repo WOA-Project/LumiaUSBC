7ffb81156c0779dc6a792522d3b3f4b16c13b9ef
Googulator <netrolller.3d@gmail.com>
2018-06-25 16:53:42 +0200

    Add project files.

---
+++ LumiaUSBCKm/Public.h
@@ -0,0 +1,24 @@
+/*++
+
+Module Name:
+
+    public.h
+
+Abstract:
+
+    This module contains the common declarations shared by driver
+    and user applications.
+
+Environment:
+
+    user and kernel
+
+--*/
+
+//
+// Define an Interface Guid so that apps can find the device and talk to it.
+//
+
+DEFINE_GUID (GUID_DEVINTERFACE_LumiaUSBCKm,
+    0xeca28b08,0x54d9,0x4c81,0x95,0xbd,0xe5,0x5c,0xf9,0xc6,0xf3,0xfd);
+// {eca28b08-54d9-4c81-95bd-e55cf9c6f3fd}
